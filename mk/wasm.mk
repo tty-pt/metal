2624c0009cf92d201972b1d30014905efe8a083a
Q <q@qnixsoft.com>
2020-06-08 00:58:24 +0100

    updated link flags & wasm2js dir

--- mk/wasm.mk
+++ mk/wasm.mk
@@ -10,19 +10,13 @@ INSTALL_BINDIR ?= ${METAL_PREFIX}/bin
 INSTALL_LIBDIR ?= ${METAL_PREFIX}/lib
 INSTALL_INCDIR ?= ${METAL_PREFIX}/include
 
-
 LLVM_ROOT:=/usr/local
 CC:=${LLVM_ROOT}/bin/clang
 CFLAGS += --sysroot ${METAL_PREFIX} --target=${TARGET}
-LDFLAGS := --allow-undefined-file=${METAL_PREFIX}/lib/wasm.syms --export-dynamic ${LDFLAGS}
-LD_CFLAGS += -fuse-ld=${LD} ${LDFLAGS:%=--Wl,%}
+LD_CFLAGS += -fuse-ld=${LD} -Wl,--allow-undefined-file=${METAL_PREFIX}/lib/wasm.syms,--export-dynamic
 
 LD:=${LLVM_ROOT}/bin/wasm-ld
-RTLIB=clang_rt.builtins-wasm32
-# LDLIBS+=-l${RTLIB}
-LINK.c += -resource-dir=${METAL_PREFIX}
-RTLIB := clang_rt.builtins-wasm32
-PROG_LDLIBS+=-lc -l${RTLIB}
+LINK.c := ${CC} ${CFLAGS} ${LD_CFLAGS} -resource-dir=${METAL_PREFIX}
 
 AR=llvm-ar
 RANLIB=llvm-ranlib
@@ -32,4 +26,4 @@ CROSS-COMPILE=llvm-
 
 .SUFFIXES: .wasm .js
 .wasm.js:
-	${BINARYEN_PATH}/wasm2js ${wasm2js-flags-y} $< > $@
+	${PREFIX}/bin/wasm2js ${wasm2js-flags-y} $< > $@
